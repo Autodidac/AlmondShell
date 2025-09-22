1726ba82f3f2e28f070d6e40cc1676b30c6252e2
Adam Rushford <177971875+Autodidac@users.noreply.github.com>
2025-09-22 10:02:55 -0400

    Align SDL and SFML state with shared window data

--- AlmondShell/include/asfmlstate.hpp
+++ AlmondShell/include/asfmlstate.hpp
@@ -27,34 +27,81 @@
 
 #if defined(ALMOND_USING_SFML)
 
+#include "arobusttime.hpp"
 #include "acontextwindow.hpp"
 
-//#include <SFML/Graphics.hpp>
-//#include <SFML/Window.hpp> // for sf::RenderWindow, sf::Event, etc.
-    
+#include <array>
+#include <bitset>
+#include <functional>
+
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Mouse.hpp>
 
 namespace almondnamespace::sfmlcontext::state
 {
-    // This is your global renderer state
     struct SFML3State
     {
-    almondnamespace::contextwindow::WindowData window{};
-        // Nothing fancy needed here yet.
-        // You can expand this with:
-        // - active shaders
-        // - viewports
-        // - default font
-        // - more later.
+        SFML3State()
+        {
+            window.width = DEFAULT_WINDOW_WIDTH;
+            window.height = DEFAULT_WINDOW_HEIGHT;
+            window.should_close = false;
+            screenWidth = window.width;
+            screenHeight = window.height;
+        }
 
-        // ✅ Safe getter, returns pointer, no UB
-    [[nodiscard]] sf::RenderWindow* get_sfml_window() noexcept
-    {
-        return window.sfml_window;
-    }
+        almondnamespace::contextwindow::WindowData window{};
 
+        sf::Event event{};
+
+        bool shouldClose{ false };
+        int screenWidth{ DEFAULT_WINDOW_WIDTH };
+        int screenHeight{ DEFAULT_WINDOW_HEIGHT };
+        bool running{ false };
+
+        std::function<void(int, int)> onResize{};
+
+        struct MouseState
+        {
+            std::array<bool, static_cast<std::size_t>(sf::Mouse::ButtonCount)> down{};
+            std::array<bool, static_cast<std::size_t>(sf::Mouse::ButtonCount)> pressed{};
+            std::array<bool, static_cast<std::size_t>(sf::Mouse::ButtonCount)> prevDown{};
+            int lastX = 0;
+            int lastY = 0;
+        } mouse{};
+
+        struct KeyboardState
+        {
+            std::bitset<sf::Keyboard::KeyCount> down;
+            std::bitset<sf::Keyboard::KeyCount> pressed;
+            std::bitset<sf::Keyboard::KeyCount> prevDown;
+        } keyboard{};
+
+        time::Timer pollTimer = time::createTimer(1.0);
+        time::Timer fpsTimer = time::createTimer(1.0);
+        int frameCount = 0;
+
+        [[nodiscard]] sf::RenderWindow* get_sfml_window() const noexcept
+        {
+            return window.sfml_window;
+        }
+
+        void mark_should_close(bool value) noexcept
+        {
+            shouldClose = value;
+            window.should_close = value;
+        }
+
+        void set_dimensions(int w, int h) noexcept
+        {
+            screenWidth = w;
+            screenHeight = h;
+            window.set_size(w, h);
+        }
     };
 
-    inline SFML3State s_sfmlstate{}; // for symmetry with other contexts
+    inline SFML3State s_sfmlstate{};
 }
 
 #endif // ALMOND_USING_SFML
